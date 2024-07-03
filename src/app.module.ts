a6787e9f1e8ce9bbc9334047afc1a69d79e7c2cc
Aleksandr <jamp.ivonin@bk.ru>
2024-07-03 10:30:53 +0300

    feat: generate new service "chat"

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ChatController } from './chat/chat.controller';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017')],
-  controllers: [AppController],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017'), ChatModule],
+  controllers: [AppController, ChatController],
   providers: [AppService],
 })
 export class AppModule {}
