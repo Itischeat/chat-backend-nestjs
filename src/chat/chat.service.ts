f59b94255560d0990f804916b181f46cdd637c60
Aleksandr <jamp.ivonin@bk.ru>
2024-07-03 10:59:05 +0300

    fix: trouble of git merge fix

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@nestjs/common';
-<<<<<<< HEAD
 import { Model } from 'mongoose';
 import { Chat } from './schemas/chat.schema';
 import { InjectModel } from '@nestjs/mongoose';
@@ -8,8 +7,3 @@ import { InjectModel } from '@nestjs/mongoose';
 export class ChatService {
   constructor(@InjectModel(Chat.name) private chatModel: Model<Chat>) {}
 }
-=======
-
-@Injectable()
-export class ChatService {}
->>>>>>> a6787e9f1e8ce9bbc9334047afc1a69d79e7c2cc
