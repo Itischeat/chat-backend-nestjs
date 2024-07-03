f35805476ebcde39b0f8331e40ff8bca630708a6
Aleksandr <jamp.ivonin@bk.ru>
2024-07-03 16:04:27 +0300

    feat: add model for User chats

---
+++ src/user/schemas/user-chat.schema.ts
@@ -0,0 +1,24 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument, Types } from 'mongoose';
+
+export type UserChatsDocument = HydratedDocument<UserChats>;
+
+@Schema()
+export class UserChats {
+  @Prop({ type: Types.ObjectId })
+  _id: Types.ObjectId;
+
+  @Prop({
+    type: Types.ObjectId,
+    ref: 'User',
+  })
+  user_id: Types.ObjectId;
+
+  @Prop({
+    type: Types.ObjectId,
+    ref: 'Chat',
+  })
+  chat_id: Types.ObjectId;
+}
+
+export const UserChatsSchema = SchemaFactory.createForClass(UserChats);
