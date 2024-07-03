0be44c553b21e3253797ccb9e0fc8750c9ebdc99
Aleksandr <jamp.ivonin@bk.ru>
2024-07-03 12:57:16 +0300

    feat: add loggin of connected and disconnected Socket

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,14 +1,27 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+} from '@nestjs/websockets';
 import { Socket } from 'socket.io';
 import { ChatService } from './chat.service';
 import { Chat } from './schemas/chat.schema';
 
 @WebSocketGateway()
-export class ChatGateway {
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   constructor(private chatService: ChatService) {}
 
   @SubscribeMessage('message')
   async handleMessage(client: Socket, payload: Chat): Promise<void> {
     await this.chatService.sendMessage(payload);
+  }
+
+  handleConnection(client: Socket) {
+    console.log(`Connected: ${client.id}`);
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log(`Disconnected: ${client.id}`);
   }
 }
